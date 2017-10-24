8f8ea04eee62923cb0b2909a6e90283bdca128a2
Aron Ahmadia <aron@ahmadia.net>
2017-10-24 19:18:27 -0400

    Add traffic flow example from last week

---
+++ traffic_flow.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+m = 400
+dx = 1.0/m
+x = np.arange(-dx/2, 1.0+dx/2, dx)
+
+t = 0
+T = 0.5
+dt = 0.9 * dx
+
+Q = 0.9*np.exp(-100*(x-0.5)**2)
+Q_new = np.empty(Q.shape)
+
+QQ = [Q]
+
+while t < T:
+    Q_new[1:-1] = 0.5 * (Q[:-2] + Q[2:]) - \
+                 0.5 * dt/dx * (Q[2:]*(1-Q[2:]) - Q[:-2]*(1-Q[:-2]))
+    Q = Q_new.copy()
+    Q[0] = Q[-2]
+    Q[-1] = Q[1]
+    t = t + dt
+    QQ.append(Q)
